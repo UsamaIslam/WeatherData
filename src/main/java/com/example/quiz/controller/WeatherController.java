58af231ffb2980b79b799b62eba7cc16083ca382
Usama Islam <usama.islam@itmaxglobal.com>
2023-12-05 01:56:26 +0500

    changed controller accordinng to new service and implemented proper logging

--- src/main/java/com/example/quiz/controller/WeatherController.java
+++ src/main/java/com/example/quiz/controller/WeatherController.java
@@ -2,15 +2,20 @@ package com.example.quiz.controller;
 
 import com.example.quiz.dto.WeatherData;
 import com.example.quiz.service.WeatherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
+
+    private static final Logger log = LoggerFactory.getLogger(WeatherController.class);
 
     private final WeatherService weatherService;
 
@@ -20,10 +25,14 @@ public class WeatherController {
     }
 
     @GetMapping
-    public Mono<WeatherData> getWeather() {
-        double latitude = 52.52; // Replace with actual latitude
-        double longitude = 13.419998; // Replace with actual longitude
-
-        return weatherService.fetchWeatherData(latitude, longitude);
+    public Mono<WeatherData> getWeather(
+            @RequestParam("latitude") double latitude,
+            @RequestParam("longitude") double longitude
+    ) {
+        return weatherService.fetchWeatherData(latitude, longitude)
+                .doOnError(error -> {
+                    // Handle errors and return a fallback or provide a meaningful error message
+                    log.error("Error fetching weather data: {}", error.getMessage(), error);
+                });
     }
 }
