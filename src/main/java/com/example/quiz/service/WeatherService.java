145ab831fa8d43091b5a9b060b1572a4763ce14a
Usama Islam <usama.islam@itmaxglobal.com>
2023-12-05 01:55:52 +0500

    changed the static longitude and latitude

--- src/main/java/com/example/quiz/service/WeatherService.java
+++ src/main/java/com/example/quiz/service/WeatherService.java
@@ -1,24 +1,38 @@
 package com.example.quiz.service;
 
 import com.example.quiz.dto.WeatherData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
+
 @Service
 public class WeatherService {
+
+    private static final Logger log = LoggerFactory.getLogger(WeatherService.class);
 
     private final WebClient webClient;
 
     public WeatherService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("https://api.open-meteo.com/v1").build();
+        this.webClient = webClientBuilder.baseUrl("https://api.open-meteo.com/v1")
+                .build();
     }
 
     public Mono<WeatherData> fetchWeatherData(double latitude, double longitude) {
         return webClient.get()
-                .uri("/forecast?latitude=52.52&longitude=13.41&current=apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure",
+                .uri("/forecast?latitude={latitude}&longitude={longitude}&current=apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure",
                         latitude, longitude)
                 .retrieve()
-                .bodyToMono(WeatherData.class);
+                .bodyToMono(WeatherData.class)
+                .timeout(Duration.ofSeconds(10))
+                .doOnError(error -> {
+                    // Handle errors and return a fallback or provide a meaningful error message
+                    log.error("Error fetching weather data: " + error.getMessage(), error);
+                })
+                .retry(3); // Retry up to 3 times in case of transient failures
     }
 }
