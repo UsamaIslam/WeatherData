7925e77dfc706936b6a7f847660a4909f2d0377c
Usama Islam <usama.islam@itmaxglobal.com>
2023-12-04 23:21:04 +0500

    Added tests

--- src/main/java/com/example/quiz/dto/WeatherData.java
+++ src/main/java/com/example/quiz/dto/WeatherData.java
@@ -30,10 +30,81 @@ public class WeatherData {
     @JsonProperty("current_units")
     private Units currentUnits;
 
+    // Getters and setters
     @JsonProperty("current")
     private CurrentWeather currentWeather;
 
-    // Getters and setters
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getGenerationTimeMs() {
+        return generationTimeMs;
+    }
+
+    public void setGenerationTimeMs(double generationTimeMs) {
+        this.generationTimeMs = generationTimeMs;
+    }
+
+    public int getUtcOffsetSeconds() {
+        return utcOffsetSeconds;
+    }
+
+    public void setUtcOffsetSeconds(int utcOffsetSeconds) {
+        this.utcOffsetSeconds = utcOffsetSeconds;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public String getTimezoneAbbreviation() {
+        return timezoneAbbreviation;
+    }
+
+    public void setTimezoneAbbreviation(String timezoneAbbreviation) {
+        this.timezoneAbbreviation = timezoneAbbreviation;
+    }
+
+    public double getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(double elevation) {
+        this.elevation = elevation;
+    }
+
+    public Units getCurrentUnits() {
+        return currentUnits;
+    }
+
+    public void setCurrentUnits(Units currentUnits) {
+        this.currentUnits = currentUnits;
+    }
+
+    public CurrentWeather getCurrentWeather() {
+        return currentWeather;
+    }
+
+    public void setCurrentWeather(CurrentWeather currentWeather) {
+        this.currentWeather = currentWeather;
+    }
 
     @Data
     public static class Units {
@@ -72,6 +143,102 @@ public class WeatherData {
 
         @JsonProperty("surface_pressure")
         private String surfacePressure;
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public String getInterval() {
+            return interval;
+        }
+
+        public void setInterval(String interval) {
+            this.interval = interval;
+        }
+
+        public String getApparentTemperature() {
+            return apparentTemperature;
+        }
+
+        public void setApparentTemperature(String apparentTemperature) {
+            this.apparentTemperature = apparentTemperature;
+        }
+
+        public String getIsDay() {
+            return isDay;
+        }
+
+        public void setIsDay(String isDay) {
+            this.isDay = isDay;
+        }
+
+        public String getPrecipitation() {
+            return precipitation;
+        }
+
+        public void setPrecipitation(String precipitation) {
+            this.precipitation = precipitation;
+        }
+
+        public String getRain() {
+            return rain;
+        }
+
+        public void setRain(String rain) {
+            this.rain = rain;
+        }
+
+        public String getShowers() {
+            return showers;
+        }
+
+        public void setShowers(String showers) {
+            this.showers = showers;
+        }
+
+        public String getSnowfall() {
+            return snowfall;
+        }
+
+        public void setSnowfall(String snowfall) {
+            this.snowfall = snowfall;
+        }
+
+        public String getWeatherCode() {
+            return weatherCode;
+        }
+
+        public void setWeatherCode(String weatherCode) {
+            this.weatherCode = weatherCode;
+        }
+
+        public String getCloudCover() {
+            return cloudCover;
+        }
+
+        public void setCloudCover(String cloudCover) {
+            this.cloudCover = cloudCover;
+        }
+
+        public String getPressureMsl() {
+            return pressureMsl;
+        }
+
+        public void setPressureMsl(String pressureMsl) {
+            this.pressureMsl = pressureMsl;
+        }
+
+        public String getSurfacePressure() {
+            return surfacePressure;
+        }
+
+        public void setSurfacePressure(String surfacePressure) {
+            this.surfacePressure = surfacePressure;
+        }
 
         // Getters and setters
     }
@@ -113,6 +280,102 @@ public class WeatherData {
 
         @JsonProperty("surface_pressure")
         private double surfacePressure;
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public int getInterval() {
+            return interval;
+        }
+
+        public void setInterval(int interval) {
+            this.interval = interval;
+        }
+
+        public double getApparentTemperature() {
+            return apparentTemperature;
+        }
+
+        public void setApparentTemperature(double apparentTemperature) {
+            this.apparentTemperature = apparentTemperature;
+        }
+
+        public int getIsDay() {
+            return isDay;
+        }
+
+        public void setIsDay(int isDay) {
+            this.isDay = isDay;
+        }
+
+        public double getPrecipitation() {
+            return precipitation;
+        }
+
+        public void setPrecipitation(double precipitation) {
+            this.precipitation = precipitation;
+        }
+
+        public double getRain() {
+            return rain;
+        }
+
+        public void setRain(double rain) {
+            this.rain = rain;
+        }
+
+        public double getShowers() {
+            return showers;
+        }
+
+        public void setShowers(double showers) {
+            this.showers = showers;
+        }
+
+        public double getSnowfall() {
+            return snowfall;
+        }
+
+        public void setSnowfall(double snowfall) {
+            this.snowfall = snowfall;
+        }
+
+        public int getWeatherCode() {
+            return weatherCode;
+        }
+
+        public void setWeatherCode(int weatherCode) {
+            this.weatherCode = weatherCode;
+        }
+
+        public int getCloudCover() {
+            return cloudCover;
+        }
+
+        public void setCloudCover(int cloudCover) {
+            this.cloudCover = cloudCover;
+        }
+
+        public double getPressureMsl() {
+            return pressureMsl;
+        }
+
+        public void setPressureMsl(double pressureMsl) {
+            this.pressureMsl = pressureMsl;
+        }
+
+        public double getSurfacePressure() {
+            return surfacePressure;
+        }
+
+        public void setSurfacePressure(double surfacePressure) {
+            this.surfacePressure = surfacePressure;
+        }
 
         // Getters and setters
     }
--- src/test/java/com/example/quiz/QuizApplicationTests.java
+++ src/test/java/com/example/quiz/QuizApplicationTests.java
@@ -1,13 +1,60 @@
 package com.example.quiz;
 
+import com.example.quiz.dto.WeatherData;
+import com.example.quiz.service.WeatherService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@AutoConfigureWebTestClient
 class QuizApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@MockBean
+	private WeatherService weatherService;
+
 	@Test
-	void contextLoads() {
+	void getWeather() {
+		double latitude = 52.52;
+		double longitude = 13.419998;
+
+		WeatherData mockWeatherData = createMockWeatherData();
+
+		when(weatherService.fetchWeatherData(anyDouble(), anyDouble()))
+				.thenReturn(Mono.just(mockWeatherData));
+
+		webTestClient.get()
+				.uri("/weather")
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.latitude").isEqualTo(latitude)
+				.jsonPath("$.longitude").isEqualTo(longitude)
+				.jsonPath("$.current.apparent_temperature").isEqualTo(mockWeatherData.getCurrentWeather().getApparentTemperature());
 	}
 
+	private WeatherData createMockWeatherData() {
+		WeatherData weatherData = new WeatherData();
+		weatherData.setLatitude(52.52);
+		weatherData.setLongitude(13.419998);
+
+		WeatherData.CurrentWeather currentWeather = new WeatherData.CurrentWeather();
+		currentWeather.setApparentTemperature(-10.6);
+
+		weatherData.setCurrentWeather(currentWeather);
+
+		return weatherData;
+	}
 }
